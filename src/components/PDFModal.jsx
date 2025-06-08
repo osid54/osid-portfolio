dc7c6db05300455323c263eedbcf6dfc7827c847
osid54 <osid.potato@gmail.com>
2025-06-08 02:09:39 -0700

    Finished modals, added metadata, removed doodle stickers

--- src/components/PDFModal.jsx
+++ src/components/PDFModal.jsx
@@ -1,34 +1,54 @@
-// PDFModal.jsx (No changes)
 import React from 'react';
 
-const PDFModal = ({ isOpen, onClose, pdfSrc, imageUrl, downloadFileName }) => {
+const PDFModal = ({ isOpen, onClose, pdfSrc, imageUrl, downloadFileName, bgUrl, downloadLabel }) => {
   if (!isOpen) return null;
 
-  const isPdf = pdfSrc && pdfSrc.toLowerCase().endsWith('.pdf');
+  const isPdfDownload = pdfSrc && pdfSrc.toLowerCase().endsWith('.pdf');
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white p-4 rounded-lg shadow-lg w-3/4 h-3/4 flex flex-col relative">
-        <button onClick={onClose} className="absolute top-2 right-2 text-gray-700 hover:text-gray-900 text-2xl font-bold">
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center"
+      style={{ backgroundColor: 'rgba(0, 0, 0, 0.8)' }}
+      onClick={onClose}
+    >
+      <div className="bg-white p-4 rounded-lg shadow-lg w-3/4 h-3/4 flex flex-col relative"
+        style={{ backgroundImage: `url(assets/papers/${bgUrl}.jpg)` }}
+        onClick={(e) => e.stopPropagation()}
+
+      >
+        <button
+          onClick={onClose}
+          className="absolute top-[3%] right-[3%] text-gray-700 hover:text-red-500 text-2xl font-bold cursor-pointer"
+        >
           &times;
         </button>
 
-        <div className="flex-grow overflow-y-auto mb-4">
-          {isPdf ? (
-            <iframe src={pdfSrc} title="PDF Viewer" className="w-full h-full border-0"></iframe>
+        <div
+          className="flex-grow overflow-y-auto mb-4 flex flex-col rounded-lg text-center"
+        >
+          {imageUrl ? (
+            <img
+              src={imageUrl}
+              alt="Document"
+              className="w-full object-contain"
+            />
           ) : (
-            <img src={imageUrl} alt="Document" className="w-full h-full object-contain" />
+            <p>No image available to display.</p>
           )}
         </div>
 
-        {(isPdf || imageUrl) && downloadFileName && (
-          <div className="flex justify-center">
+        {(isPdfDownload || imageUrl) && downloadFileName && (
+          <div className="flex justify-center h-[20vh] items-center">
             <a
-              href={isPdf ? pdfSrc : imageUrl}
+              href={isPdfDownload ? pdfSrc : imageUrl}
               download={downloadFileName}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              className="flex bg-gray-400 rounded-full hover:bg-gray-300 font-semibold text-3xl h-[80%] w-1/2 justify-center items-center text-center cursor-pointer"
+              style={{ fontFamily: 'Architex', backgroundBlendMode: 'overlay' }}
             >
-              Download {downloadFileName}
+              <span 
+              style={{ position: 'relative', top: '5px' }}>
+                Download {downloadLabel}
+              </span>
             </a>
           </div>
         )}
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Learn about Omar Siddiqui, aspiring software engineer." />
+
+        <meta property="og:title" content="Omar Siddiqui's Portfolio" />
+        <meta property="og:description" content="Learn about Omar Siddiqui, aspiring software engineer." />
+        <meta property="og:url" content="https://www.osid.dev" />
+        <meta property="og:site_name" content="Omar Siddiqui's Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://www.osid.dev/assets/osid-portfolio.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="en_US" />
+      </Head>
       <body>
         <Main />
         <NextScript />
