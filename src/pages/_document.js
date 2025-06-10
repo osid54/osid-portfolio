4e0dc058055b4239702adfb2f45073486b1b90f2
osid54 <osid.potato@gmail.com>
2025-06-10 13:15:17 -0700

    Went back to Next import instead of React

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Document() {
   return (
